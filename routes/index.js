bff00e4a9235eed869b30d30c2ed461153a81b88
User Name <victoriamurra30@gmail.com>
2025-04-22 13:24:36 -0400

    updated events

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userRouter from "./user.js";
+import eventRouter from "./event.js"; 
 
 const router = Router();
 
-router.get("/", (req, res)=> {
-    res.send("This is the landing page")
-})
+router.get("/", (req, res) => {
+  res.send("This is the landing page");
+});
 
 router.use("/users", userRouter);
+router.use("/events", eventRouter); 
 
 export default router;
