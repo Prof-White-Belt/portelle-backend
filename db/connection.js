16528252c95445ae61c5741f9a4af2f8969c9fed
StellaTeuscher <stella.teuscher@gmail.com>
2025-04-22 10:12:35 -0400

    db set up

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,19 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+
+mongoose.set("returnOriginal", false);
+
+mongoose.connect(process.env.MONGODB_URI).catch((err) => {
+    console.log(`Error connection to MongoDB: ${err.message}`)
+})
+
+mongoose.connection.on("disconnect", () => {
+    console.log("Disconnected from DB");
+} )
+
+mongoose.connection.on("error", (err) => {
+    console.log("MongoDB contains error: " + err);
+
+})
+
+export default mongoose.connect;
--- server.js
+++ server.js
@@ -2,3 +2,4 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import logger from "morgan";
+import db from "./db/connection.js";
